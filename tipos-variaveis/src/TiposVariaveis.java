7b09a35c3f1a27ae4157cea9ee5fb8c5bddb0fde
Daniel Junior <dap.junioruvv@gmail.com>
2023-02-28 16:34:22 -0300

    tipos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        // tipos primitivos
+        
+        String meuNome = "Daniel";
+
+        double salario = 2000;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        final int NUM = 10; // constante
+
+        
     }
 }
